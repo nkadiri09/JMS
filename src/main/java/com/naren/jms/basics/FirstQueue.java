339ddce58c2ae1a3c2802405033f80a959fae3a3
Narendra <nkadiri09@gmail.com>
2020-01-15 01:02:46 -0600

    Chore: added producer and consumer

--- src/main/java/com/naren/jms/basics/FirstQueue.java
+++ src/main/java/com/naren/jms/basics/FirstQueue.java
@@ -8,20 +8,42 @@ public class FirstQueue {
 
     public static void main(String[] args)  {
         InitialContext initialContext= null;
+        Connection connection =null;
         try{
-        initialContext= new InitialContext();
+            initialContext = new InitialContext();
             ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
-            Connection connection = connectionFactory.createConnection();
+            connection = connectionFactory.createConnection();
             Session session = connection.createSession();
             Queue queue = (Queue) initialContext.lookup("queue/myQueue");
             MessageProducer producer = session.createProducer(queue);
             TextMessage message = session.createTextMessage("hello JMS world!");
             producer.send(message);
+            System.out.println("Message sent: "+message.getText());
+
+            MessageConsumer consumer = session.createConsumer(queue);
+            connection.start();
+            TextMessage messageReceived = (TextMessage) consumer.receive();
+            System.out.println("Message Received: "+messageReceived.getText());
 
         } catch (NamingException e){
             e.printStackTrace();
         } catch (JMSException e) {
             e.printStackTrace();
+        }finally {
+            if(initialContext!=null){
+                try {
+                    initialContext.close();
+                }catch (NamingException e){
+                    e.printStackTrace();
+                }
+            }
+            if(connection!=null){
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
